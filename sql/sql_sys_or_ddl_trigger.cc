0a40a4e60dcdc9a8109a6c6658d6878ed5f8d43a
Dmitry Shulga <dmitry.shulga@mariadb.com>
2026-05-20 14:31:31 +0700

    MDEV-30645: CREATE TRIGGER FOR { STARTUP | SHUTDOWN }
    Follow-up to fix building with EMBEDDED server. Since support of
    events isn't compiled in for embedded server but some stuff from
    implementation of events is used for support of triggers, common
    source code used both for events and triggers was extracted into
    the separate files event_common.cc/event_common.h

--- sql/sql_sys_or_ddl_trigger.cc
+++ sql/sql_sys_or_ddl_trigger.cc
@@ -6,10 +6,11 @@
 
 #include "table.h"
 
-#include "events.h"
+#include "event_common.h"         // Event_creation_ctx
 #include "event_data_objects.h"   // load_creating_context_for_sys_trg
 #include "event_db_repository.h"  // enum_events_table_field
 #include "event_parse_data.h"     // Event_parse_data
+#include "events.h"
 
 #include "key.h"                  // key_copy
 #include "lex_string.h"
@@ -818,9 +819,12 @@ static LEX_CSTRING events_to_string(const LEX_CSTRING base_event_names[],
       offset+= sprintf(set_of_events + offset, "%s,",
                        base_event_names[idx].str);
   }
-  set_of_events[offset - 1]= 0;
-
-  return LEX_CSTRING{set_of_events, offset - 1};
+  if (offset)
+  {
+    set_of_events[offset - 1]= 0;
+    offset= offset - 1;
+  }
+  return LEX_CSTRING{set_of_events, offset};
 }
 
 
@@ -1139,12 +1143,12 @@ static bool load_system_triggers(THD *thd)
   {
     /*
       In case events scheduler is enabled (in that case
-      Events::startup_state == Events::EVENTS_ON) warning about
+      Events_common::startup_state == Events::EVENTS_ON) warning about
       incorrect layout of the table mysql.event has been already reported
       on loading events (@see Events::init), so don't print the error
       message.
     */
-    if (Events::startup_state != Events::EVENTS_ON)
+    if (Events_common::startup_state != Events_common::EVENTS_ON)
       my_message(ER_STARTUP,
                  "An error occurred when loading data from "
                  "the table mysql.event. System triggers not loaded",
